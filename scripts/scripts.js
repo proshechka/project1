863fcc2edd8660f7fa9fa04402517769463819c8
proshechka <ihalturin19@gmail.com>
2025-03-12 16:30:06 +0300

    css

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function () {
+    const preloader = document.querySelector('.preloader');
+    const content = document.querySelector('.content');
+  
+    preloader.classList.add('hidden');
+  
+    setTimeout(() => {
+      content.classList.add('visible');
+    }, 800);
+  });
+
+  
